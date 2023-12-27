7f4769e6e719c47423b4c4451456101e86df5f23
Toby Scott <hi@tobyscott.dev>
2023-12-27 19:39:43 +1100

    Install react-router-dom in parent app

--- parent/src/App.tsx
+++ parent/src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box, Flex, Text, Image } from "@chakra-ui/react";
 
 import ErrorBoundary from "./components/ErrorBoundary";
@@ -10,16 +11,28 @@ const CounterButton = React.lazy(() => import("mfe1/CounterButton"));
 
 function App() {
   return (
-    <Box width={500} mx={"auto"}>
-      <Flex justifyContent={"space-around"}>
-        <Image width={100} src={viteLogo} alt="Vite logo" />
-        <Image width={100} src={reactLogo} alt="React logo" />
-      </Flex>
-      <Text fontSize={"2xl"}>Parent app</Text>
-      <ErrorBoundary>
-        <CounterButton label={"Hello MFE"} />
-      </ErrorBoundary>
-    </Box>
+    <BrowserRouter>
+      <Box width={500} mx={"auto"}>
+        <Flex justifyContent={"space-around"}>
+          <Image width={100} src={viteLogo} alt="Vite logo" />
+          <Image width={100} src={reactLogo} alt="React logo" />
+        </Flex>
+        <Text fontSize={"2xl"}>Parent app</Text>
+        <Routes>
+          <Route index element={<Box>Visit /mfe1/*</Box>} />
+          <Route
+            path="mfe1/*"
+            element={
+              <>
+                <ErrorBoundary>
+                  <CounterButton label={"Hello MFE"} />
+                </ErrorBoundary>
+              </>
+            }
+          />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   );
 }
 
